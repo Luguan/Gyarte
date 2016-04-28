0e246f119a9f3850933e770e06de598481b9ecc5
Luguan <lulleannsberg@gmail.com>
2016-04-28 22:10:55 +0200

    working login, working on register styling

--- Gyarte/html/web.js
+++ Gyarte/html/web.js
@@ -1,7 +1,8 @@
-const element = document.querySelector("#loginForm");
+const login = document.querySelector("[name=login]");
+const register = document.querySelector("[name=register]");
 
-
-element.addEventListener('submit',function(e){
+register.style.display = "none";
+login.addEventListener('submit',function(e){
   e.preventDefault();
   const username = document.querySelector("[name=username]").value;
   const password = document.querySelector("[name=password]").value;
@@ -15,6 +16,12 @@ function sendLoginInfo(name, pw) {
     if (xhttp.readyState === 4) {
       if(xhttp.status === 200) {
         alert(xhttp.responseText);
+        const response = JSON.parse(xhttp.responseText);
+        if(response.loggedIn){
+          alert("funkar!");
+          login.style.display = "none";
+          register.style.display = "block";
+        }
       }
       else {
         alert("not working");
@@ -25,6 +32,7 @@ function sendLoginInfo(name, pw) {
     }
   };
   xhttp.open("POST", "/login", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
   xhttp.send(JSON.stringify({
     "username": name,
     "password": pw
